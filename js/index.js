65e5320a9f8fd79bb9e3b80bf3f0bf6f044c1d9c
abonla <abon8820@gmail.com>
2021-12-28 15:58:16 +0800

    取消載入畫面

--- js/index.js
+++ js/index.js
@@ -317,25 +317,25 @@ $(document).ready(function () {
 
 // loading page
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-	var counter = 0;
-	var c = 0;
-	var i = setInterval(function () {
-		$(".loading-page .counter h1").html(c + "%");
-		$(".loading-page .counter hr").css("width", c + "%");
+// 	var counter = 0;
+// 	var c = 0;
+// 	var i = setInterval(function () {
+// 		$(".loading-page .counter h1").html(c + "%");
+// 		$(".loading-page .counter hr").css("width", c + "%");
 
-		counter++;
-		c++;	
+// 		counter++;
+// 		c++;	
 
-		if (counter == 101) {
-			clearInterval(i);
-		}}, 50);
+// 		if (counter == 101) {
+// 			clearInterval(i);
+// 		}}, 50);
 		
-	$(window).load(function () {
-		$('.loading-page').fadeOut(300);
-	});
+// 	$(window).load(function () {
+// 		$('.loading-page').fadeOut(300);
+// 	});
 
-});
+// });
 
 
