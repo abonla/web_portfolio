c5e8781ffa66041b4bc255060bbb1962a298de75
abonla <abon8820@gmail.com>
2021-12-29 14:52:23 +0800

    把loadpage script放在index.html

--- js/index.js
+++ js/index.js
@@ -315,25 +315,5 @@ $(document).ready(function () {
 
 
 
-// loading page
-
-$(document).ready(function () {
-
-	var counter = 0;
-	var c = 0;
-	var i = setInterval(function () {
-		$(".loading-page .counter h1").html(c + "%");
-		$(".loading-page .counter hr").css("width", c + "%");
-
-		counter++;
-		c++;	
-
-		if (counter == 101) {
-			clearInterval(i);
-			$('.loading-page').fadeOut(300);
-		}}, 50);
-		
-
-});
 
 
